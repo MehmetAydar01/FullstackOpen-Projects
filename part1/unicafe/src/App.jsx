fc2b3046c0050208f5c13e29785ff906dc6f4866
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-07 18:21:51 +0300

    unicafe genel yapi duzenlendi

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,59 +1,43 @@
 import { useState } from 'react';
 
-const Header = ({ text }) => <h1>{text}</h1>;
 
-const Button = ({ text, setFunction, value }) => {
-  return <button onClick={() => setFunction(value + 1)}>{text}</button>;
-};
 
-const Buttons = ({ statistics, setFunctions }) => {
-  return (
-    <div>
-      <Button
-        text='good'
-        setFunction={setFunctions[0]}
-        value={statistics.good}
-      />
-      <Button
-        text='neutral'
-        setFunction={setFunctions[1]}
-        value={statistics.neutral}
-      />
-      <Button text='bad' setFunction={setFunctions[2]} value={statistics.bad} />
-    </div>
-  );
-};
-
-const StatisticLine = ({ text, value }) => {
+const StatisticLine = ({ label, value }) => {
   return (
     <tr>
-      <td>{text}</td>
-      <td>
-        {value} {text === 'positive' && '%'}
-      </td>
+      <td>{label}</td>
+      <td>{value}</td>
     </tr>
   );
 };
 
-const Statistics = ({ statistics }) => {
-  const { good, neutral, bad, total, avg, positiveFeedbackPercentage } =
-    statistics;
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + bad + neutral;
 
   if (total === 0) {
-    return <p>No feedback given</p>;
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <div>no feedback given</div>
+      </div>
+    );
   }
 
   return (
-    <table>
-      <tbody>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={total} />
-        <StatisticLine text='average' value={avg} />
-        <StatisticLine text='positive' value={positiveFeedbackPercentage} />
-      </tbody>
-    </table>
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine label='good' value={good} />
+          <StatisticLine label='neutral' value={neutral} />
+          <StatisticLine label='bad' value={bad} />
+          <StatisticLine label='all' value={total} />
+          <StatisticLine label='average' value={(good - bad) / total} />
+          <StatisticLine label='positive' value={(100 * good) / total + ' %'} />
+        </tbody>
+      </table>
+    </div>
   );
 };
 
@@ -62,26 +46,15 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const total = good + neutral + bad;
-  const avg = (good * 1 + neutral * 0 + bad * -1) / total;
-  const positiveFeedbackPercentage = (good / total) * 100 || 0;
-
-  const statistics = {
-    good,
-    neutral,
-    bad,
-    total,
-    avg,
-    positiveFeedbackPercentage,
-  };
-  const setFunctions = [setGood, setNeutral, setBad];
-
   return (
     <div>
-      <Header text='give feedback' />
-      <Buttons statistics={statistics} setFunctions={setFunctions} />
-      <h2>statistics</h2>
-      <Statistics statistics={statistics} setFunctions={setFunctions} />
+      <h2>Give feedback</h2>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
