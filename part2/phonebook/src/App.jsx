b7670282450035239c2456d7bab9ec3ecae99153
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-12-21 22:34:32 +0300

    general arrangements were made

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-import personService from './services/persons'
+import personService from './services/persons';
 import Notification from './components/Notification';
 
 const App = () => {
@@ -15,12 +15,38 @@ const App = () => {
 
   useEffect(() => {
     // A GET request is made to the server.
-    personService
-      .getAllPerson()
-      .then((initialData) => {
-        setPersons(initialData);
-      });
+    personService.getAllPerson().then((initialData) => {
+      setPersons(initialData);
+    });
   }, []);
+
+  const updatePerson = () => {
+    const ok = window.confirm(
+      `${newName} is already added to phonebook, replace the old number with a new one?`
+    );
+
+    if (ok) {
+      const findPerson = persons.find((person) => person.name === newName);
+      const updatePersonInfo = { ...findPerson, number: newNumber };
+
+      // A PUT request is made to the server.
+      personService
+        .updatePerson(findPerson.id, updatePersonInfo)
+        .then((returnedUpdatedData) => {
+          setPersons(
+            persons.map((item) =>
+              item.id === findPerson.id ? returnedUpdatedData : item
+            )
+          );
+          setSuccessMessage(`Number updated`);
+        })
+        .catch((err) => {
+          setErrorMessage(
+            `Information of ${findPerson.name} has already been removed from server`
+          );
+        });
+    }
+  };
 
   // Add Person
   const addPerson = (event) => {
@@ -28,87 +54,61 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
-      // id keyi koymadım, JSON Server kendisi otomatik id atıyor.
     };
 
     const hasPerson = persons.some((person) => person.name === newName);
 
     if (hasPerson) {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        const findPerson = persons.find((person) => person.name === newName);
-        const updatePersonInfo = {...findPerson, number: newNumber}
-
-        // A PUT request is made to the server.
-        personService
-          .updatePerson(findPerson.id, updatePersonInfo)
-          .then((returnedUpdatedData) => {
-            setPersons(persons.map((item) => item.id === findPerson.id ? returnedUpdatedData : item))
-            setSuccessMessage(`Number updated`)
-          })
-          .catch((err) => {
-            setErrorMessage(`Information of ${findPerson.name} has already been removed from server`)
-          })
-      }
+      updatePerson();
     } else {
       // A POST request is made to the server.
-      personService
-        .createPerson(newPerson)
-        .then((returnedPerson) => {
-          setPersons(persons.concat(returnedPerson));
-          setSuccessMessage(`Added ${returnedPerson.name}`)
-        })
+      personService.createPerson(newPerson).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setSuccessMessage(`Added ${returnedPerson.name}`);
+      });
     }
 
     setTimeout(() => {
-      setSuccessMessage(null)
-      setErrorMessage(null)
-    }, 3000)
+      setSuccessMessage(null);
+      setErrorMessage(null);
+    }, 3000);
     setNewName('');
     setNewNumber('');
   };
 
   // Delete Person
-  const deletePerson = (id)  => {
-    const findPerson = persons.find(person => person.id === id)
+  const deletePerson = (id) => {
+    const findPerson = persons.find((person) => person.id === id);
+    const deleteOk = window.confirm(`Delete ${findPerson.name}?`);
 
-    if(window.confirm(`Delete ${findPerson.name}?`)) {
+    if (deleteOk) {
       // A DELETE request is made to the server.
       personService
         .deletePerson(id)
         .then((deletedPerson) => {
-          setSuccessMessage(
-            `${findPerson.name} was deleted`
-          )
-        }).catch((err) => {
+          setSuccessMessage(`${findPerson.name} was deleted`);
+        })
+        .catch((err) => {
           setErrorMessage(
             `the person ${findPerson.name} was already deleted from server`
-          )
-        })
+          );
+        });
 
-      setPersons(persons.filter((item) => item.id !== id))
+      setPersons(persons.filter((item) => item.id !== id));
     }
-
 
     setTimeout(() => {
-      setSuccessMessage(null)
-      setErrorMessage(null)
-    }, 3000)
-  }
-
-  /* 
-    ,
-    {
-      "id": "f580",
-      "name": "QXyGeN Roshan",
-      "number": "01-56-3334197"
-    }
-  */
+      setSuccessMessage(null);
+      setErrorMessage(null);
+    }, 3000);
+  };
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
   const handleNumberChange = (event) => setNewNumber(event.target.value);
 
-  const handleSearchPersonChange = (event) => setSearchPerson(event.target.value);
+  const handleSearchPersonChange = (event) =>
+    setSearchPerson(event.target.value);
 
   // Search Person
   const filteredPersons = persons.filter((person) =>
@@ -118,7 +118,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification successMessage={successMessage} errorMessage={errorMessage} />
+      <Notification
+        successMessage={successMessage}
+        errorMessage={errorMessage}
+      />
       <Filter
         searchPerson={searchPerson}
         handleSearchPersonChange={handleSearchPersonChange}
